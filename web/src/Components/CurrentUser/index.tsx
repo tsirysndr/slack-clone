947be2f5ffcf2419ef0d5c48af4fbd9d26656d0d
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2021-09-23 02:24:26 +0300

    web: show username info

--- web/src/Components/CurrentUser/index.tsx
+++ web/src/Components/CurrentUser/index.tsx
@@ -1,9 +1,28 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
+import { UserContext } from '../../Providers/UserProvider';
+import styled from 'styled-components';
+
+const Username = styled.div`
+  font-size: 15px;
+  font-weight: 500;
+  color: #ffffffa6;
+`;
 
 const CurrentUser: FC = () => {
+  const { user } = useContext(UserContext);
   return (
-    <div style={{ fontWeight: 'bold', fontSize: 25, padding: 25 }}>
-      Tsiry Sandratraina
+    <div
+      style={{
+        fontWeight: 'bold',
+        fontSize: 25,
+        padding: 25,
+        paddingBottom: 25,
+      }}
+    >
+      <div>
+        {user?.firstName} {user?.lastName}
+      </div>
+      <Username>@{user?.username}</Username>
     </div>
   );
 };

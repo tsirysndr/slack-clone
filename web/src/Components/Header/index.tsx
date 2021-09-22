1620c500c2d09fde8383b211ba8b02b61e7aa1eb
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2021-09-22 03:12:51 +0300

    web: split into components

--- web/src/Components/Header/index.tsx
+++ web/src/Components/Header/index.tsx
@@ -1,9 +1,25 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 
 const Header: FC = () => {
   return (
-    <div></div>
-  )
-}
+    <div
+      style={{
+        flexDirection: 'row',
+        display: 'flex',
+        fontSize: 20,
+        padding: 20,
+        borderBottom: '1px solid #0000001c',
+      }}
+    >
+      <div style={{ flex: 1, fontWeight: 'bold' }}># aléatoire</div>
+      <div
+        className="button"
+        style={{ fontWeight: 500, paddingRight: 10, cursor: 'pointer' }}
+      >
+        Logout
+      </div>
+    </div>
+  );
+};
 
 export default Header;
--- web/src/Components/MessageView/index.tsx
+++ web/src/Components/MessageView/index.tsx
@@ -1,44 +1,18 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
+import Header from '../Header';
+import Message from '../Message';
 
 const MessageView: FC = () => {
   return (
     <div style={{ flex: 1 }}>
-      <div
-        style={{
-          flexDirection: 'row',
-          display: 'flex',
-          fontSize: 20,
-          padding: 20,
-          borderBottom: '1px solid #0000001c',
-        }}
-      >
-        <div style={{ flex: 1, fontWeight: 'bold' }}># aléatoire</div>
-        <div
-          className="button"
-          style={{ fontWeight: 500, paddingRight: 10, cursor: 'pointer' }}
-        >
-          Logout
-        </div>
-      </div>
+      <Header />
       <div
         style={{
           overflowY: 'scroll',
           height: 'calc(100vh - (66px + 56px + 25px))',
         }}
       >
-        <div style={{ padding: 15 }}>
-          <div style={{ fontWeight: 'bold' }}>Tsiry Sandratraina</div>
-          <div style={{ padding: 5 }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </div>
-        </div>
-
+        <Message />
         <div style={{ padding: 15, fontWeight: 500 }}>
           <div style={{ fontWeight: 'bold' }}>Koto</div>
           <div style={{ padding: 5 }}>

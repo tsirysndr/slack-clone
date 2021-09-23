1c87a0ebb75b1c29913dba04faa55579aa9c4464
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2021-09-23 19:24:45 +0300

    api: fix allMessages query wrong results

--- graphql/src/schema/Message/Query/index.ts
+++ graphql/src/schema/Message/Query/index.ts
@@ -12,19 +12,28 @@ export const MessageQuery = extendType({
       },
       resolve: (_, args: any, ctx: Context) => {
         if (args.channelId) {
-          return ctx.prisma.message.findMany({ where: { channelId: args.channelId } });
+          return ctx.prisma.message.findMany({
+            where: { channelId: args.channelId },
+          });
         }
-        return ctx.prisma.message.findMany({ where: { receiverId: args.receiverId } });
+        if (ctx.currentUser?.id === args.receiverId) {
+          return ctx.prisma.message.findMany({
+            where: { senderId: args.receiverId, receiverId: args.receiverId },
+          });
+        }
+        return ctx.prisma.message.findMany({
+          where: { receiverId: args.receiverId },
+        });
       },
     });
     t.field('findMessage', {
       type: 'message',
       args: {
-        id: nonNull(stringArg())
+        id: nonNull(stringArg()),
       },
       resolve: (_, { id }, ctx: Context) => {
         return ctx.prisma.message.findFirst({ where: { id } });
-      }
+      },
     });
   },
 });
